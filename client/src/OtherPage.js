dd291aecd18ac6f2251ac36dd222c5921bb2db4e
SangWook <imcool2551@gmail.com>
2021-01-17 20:10:12 +0900

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      In some otehr page!
+      <Link to="/">Go back Home</Link>
+    </div>
+  );
+};
